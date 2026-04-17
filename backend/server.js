bd13276a29d278fbb01ea4bcbc31d40761e7b661
Dunstun <wambutsidunstun@gmail.com>
2026-04-17 10:06:44 +0300

    fix: resolve Cross-Origin-Opener-Policy for OAuth popups

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.set('timeout', 60000);
 app.use(helmet({
   contentSecurityPolicy: false,
   crossOriginEmbedderPolicy: { policy: "unsafe-none" },
-  crossOriginOpenerPolicy: { policy: "unsafe-none" },
+  crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" },
   crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
 app.use(compression());
