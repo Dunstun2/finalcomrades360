e142c3edf422d301ef7c4387d57dff3825587716
Dunstun <wambutsidunstun@gmail.com>
2026-05-26 18:33:28 +0300

    Make categoryId nullable migration MySQL-compatible

--- backend/migrations/20260528000000-alter-deletedproduct-categoryId-allow-null.js
+++ backend/migrations/20260528000000-alter-deletedproduct-categoryId-allow-null.js
@@ -1,11 +1,11 @@
 // Migration to allow NULL for categoryId in DeletedProduct table
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Use raw query to modify column constraint
-    await queryInterface.sequelize.query('ALTER TABLE "DeletedProducts" ALTER COLUMN "categoryId" DROP NOT NULL;');
+    // Make categoryId nullable using Sequelize API (MySQL compatible)
+    await queryInterface.changeColumn('DeletedProducts', 'categoryId', { type: Sequelize.INTEGER, allowNull: true });
   },
   down: async (queryInterface, Sequelize) => {
-    // Revert: make categoryId NOT NULL again (assuming no rows have NULL)
-    await queryInterface.sequelize.query('ALTER TABLE "DeletedProducts" ALTER COLUMN "categoryId" SET NOT NULL;');
+    // Revert to NOT NULL constraint
+    await queryInterface.changeColumn('DeletedProducts', 'categoryId', { type: Sequelize.INTEGER, allowNull: false });
   }
 };
